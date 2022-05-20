34ff85b8d7f060371a3092084020e01c30025367
jolebowski <jordan.moreira@epitech.eu>
2022-05-20 07:13:49 +0200

    update movies files

---
+++ src/data/movies.js
@@ -0,0 +1,86 @@
+const movies = [
+  {
+    id: "1",
+    title: "Oceans 8",
+    category: "Comedy",
+    likes: 4,
+    dislikes: 1,
+    imageMovie: require("../Assets/Images/oceans8.jpeg"),
+  },
+  {
+    id: "2",
+    title: "Midnight Sun",
+    category: "Comedy",
+    likes: 2,
+    dislikes: 0,
+    imageMovie: require("../Assets/Images/midnightSun.jpeg"),
+  },
+  {
+    id: "3",
+    title: "Les indestructibles 2",
+    category: "Animation",
+    likes: 3,
+    dislikes: 1,
+    imageMovie: require("../Assets/Images/indestructibles2.jpeg"),
+  },
+  {
+    id: "4",
+    title: "Sans un bruit",
+    category: "Thriller",
+    likes: 6,
+    dislikes: 6,
+    imageMovie: require("../Assets/Images/sansUnBruit.jpeg"),
+  },
+  {
+    id: "5",
+    title: "Creed II",
+    category: "Drame",
+    likes: 16,
+    dislikes: 2,
+    imageMovie: require("../Assets/Images/creed2.jpeg"),
+  },
+  {
+    id: "6",
+    title: "Pulp Fiction",
+    category: "Thriller",
+    likes: 11,
+    dislikes: 3,
+    imageMovie: require("../Assets/Images/pulpFiction.jpeg"),
+  },
+  {
+    id: "7",
+    title: "Pulp Fiction",
+    category: "Thriller",
+    likes: 12333,
+    dislikes: 32,
+    imageMovie: require("../Assets/Images/pulpFiction.jpeg"),
+  },
+  {
+    id: "8",
+    title: "Seven",
+    category: "Thriller",
+    likes: 2,
+    dislikes: 1,
+    imageMovie: require("../Assets/Images/seven.jpeg"),
+  },
+  {
+    id: "9",
+    title: "Inception",
+    category: "Thriller",
+    likes: 2,
+    dislikes: 1,
+    imageMovie: require("../Assets/Images/inception.jpeg"),
+  },
+  {
+    id: "10",
+    title: "Gone Girl",
+    category: "Thriller",
+    likes: 22,
+    dislikes: 12,
+    imageMovie: require("../Assets/Images/goneGirl.jpeg"),
+  },
+];
+
+export const movies$ = new Promise((resolve, reject) =>
+  setTimeout(resolve, 100, movies)
+);
