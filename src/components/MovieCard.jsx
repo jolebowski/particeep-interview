dd193784d22eb029a18ea65efe15d211136e39dc
jolebowski <jordan.moreira@epitech.eu>
2022-05-20 18:21:16 +0200

    add expression statement for like and dislike

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,33 +1,94 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { RemoveMovieAction } from "../redux/Actions";
 
 export const MovieCard = ({ movie }) => {
+  const [like, setLike] = useState(movie.likes);
+  const [dislikes, setDislikes] = useState(movie.dislikes);
+
+  const [likeActive, setLikeActive] = useState(false);
+  const [dislikeActive, setDisLikeActive] = useState(false);
+
+  //console.log(like, "like");
   const dispatch = useDispatch();
+  const likeButton = () => {
+    if (likeActive) {
+      setLikeActive(false);
+      setLike(like - 1);
+    } else {
+      setLikeActive(true);
+      setLike(like + 1);
+      if (dislikeActive) {
+        setDisLikeActive(false);
+        setLike(like + 1);
+        setDislikes(dislikes - 1);
+      }
+    }
+  };
+
+  const dislikeButton = () => {
+    if (dislikeActive) {
+      setDisLikeActive(false);
+      setDislikes(dislikes - 1);
+    } else {
+      setDisLikeActive(true);
+      setDislikes(dislikes + 1);
+      if (likeActive) {
+        setLikeActive(false);
+        setDislikes(dislikes + 1);
+        setLike(like - 1);
+      }
+    }
+  };
+
   return (
     <>
       <div className="col-md-4 col-sm-6 col-xs-12" key={movie.id}>
         <div className="card-header">
-          <img src={movie.imageMovie} alt="test1" />
+          <img src={movie.imageMovie} alt={movie.title} />
         </div>
-        <div className="progress" style={{ height: "2px" }}>
+        <div className="progress">
           <div
             className="progress-bar"
             role="progressbar"
-            style={{ width: "50%" }}
-            aria-valuenow="25"
+            style={{ width: `${like * 10}%`, backgroundColor: "#49a078" }}
+            aria-valuenow={like}
             aria-valuemin="0"
             aria-valuemax="100"
-          ></div>
+          >
+            {`${like}`}
+          </div>
+          <div
+            className="progress-bar"
+            role="progressbar"
+            style={{ width: `${dislikes * 10}%`, backgroundColor: "#d72638" }}
+            aria-valuenow={dislikes}
+            aria-valuemin="0"
+            aria-valuemax="100"
+          >
+            {`${dislikes}`}
+          </div>
         </div>
         <div className="card-body">
           <i
             className="bi bi-hand-thumbs-up-fill"
             style={{
               fontSize: "2rem",
-              color: "green",
+              color: "#49a078",
               cursor: "pointer",
               marginBottom: 10,
             }}
+            onClick={() => likeButton()}
+          ></i>
+          <i
+            className="bi bi-hand-thumbs-down-fill"
+            style={{
+              fontSize: "2rem",
+              color: "#d72638",
+              cursor: "pointer",
+              marginBottom: 10,
+            }}
+            onClick={() => dislikeButton()}
           ></i>
           <h2 className="card-title">{movie.title}</h2>
           <p className="card-text">{movie.category}</p>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,26 +1,36 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMoviesAction } from '../redux/Actions';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMoviesAction } from "../redux/Actions";
 import { MovieCard } from "./MovieCard";
 import { movies$ } from "../data/movies";
 
 export const Movies = () => {
-	const { movies } = useSelector((state) => state.movies);
-	const dispatch = useDispatch();
+  const { movies } = useSelector((state) => state.movies);
+  const dispatch = useDispatch();
 
   const fetchMovies = async () => {
-			const data = await movies$;
-			dispatch(fetchMoviesAction(data));
-	};
+    const data = await movies$;
+    dispatch(fetchMoviesAction(data));
+  };
 
   useEffect(() => {
-    fetchMovies()
+    fetchMovies();
   }, []);
 
   return (
     <div className="container">
-      <h1 className="mt-4 mb-4 text-center">Films</h1>
-      <div className="row">{movies && movies.map((movie) => <MovieCard movie={movie} />)}</div>
+      {movies.length !== 0 ? (
+        <h1 className="mt-4 mb-4 text-center">Films</h1>
+      ) : (
+        ""
+      )}
+      <div className="row">
+        {movies.length !== 0 ? (
+          movies.map((movie) => <MovieCard movie={movie} />)
+        ) : (
+          <h4 className="mt-4 mb-4 text-center">Aucun film</h4>
+        )}
+      </div>
     </div>
   );
 };
